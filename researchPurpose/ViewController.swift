8751e6d0cd0dad78204d769d5bc64a2375055a1a
gerryfad <gerryfadlurahman@gmail.com>
2025-03-02 16:57:16 +0700

    Initial Commit

---
+++ researchPurpose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  researchPurpose
+//
+//  Created by Suitmedia on 02/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
